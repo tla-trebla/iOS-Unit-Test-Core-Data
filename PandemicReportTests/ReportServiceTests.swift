5d13e49e76074cd24860fe771a05c6e7bf7b0a40
Albert Pangestu <stamina.left@gmail.com>
2021-11-16 20:03:59 +0700

    Added properties named "reportService" & "coreDataStack".

--- PandemicReportTests/ReportServiceTests.swift
+++ PandemicReportTests/ReportServiceTests.swift
@@ -35,6 +35,9 @@ import XCTest
 import CoreData
 
 class ReportServiceTests: XCTestCase {
+  
+  var reportService: ReportService!
+  var coreDataStack: CoreDataStack!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
